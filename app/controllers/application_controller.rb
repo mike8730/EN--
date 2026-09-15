d8aefd4240f3f39c8c51fc523bab21c73e33da55
mike8730 <razerrush5v@gmail.com>
2026-09-15 11:17:20 +0900

    Deviseのstrong parametersを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :name,
+      :gender,
+      :birthday,
+      :avatar,
+      :introduction,
+      :group_role,
+      :preferred_age_range
+    ])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :name,
+      :gender,
+      :birthday,
+      :avatar,
+      :introduction,
+      :group_role,
+      :preferred_age_range
+    ])
+  end
 end
+
