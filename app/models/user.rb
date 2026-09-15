332eaee1d0b3e6540290799048a5a1f315c4d61b
mike8730 <razerrush5v@gmail.com>
2026-09-15 11:36:31 +0900

    Userモデルに必須バリデーションとenumを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  enum gender: { male: 0, female:1 }
+  enum group_role: { member: 0, organizer: 1 } 
+
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :birthday, presence: true
+  validates :introduction, length: { maximum: 200 }
+  validates :group_role, inclusion: { in: group_roles.keys }, allow_nil: true
+  validates :preferred_age_range, format: { with: /\A\d{2}-\d{2}\z/ }, allow_nil: true
 end
